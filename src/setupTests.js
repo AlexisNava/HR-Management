501b38042f1c8fa24008e95193687215f44e8bb9
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-12 14:29:41 -0500

    Added setupTests.js file to change the tests configuration

---
+++ src/setupTests.js
@@ -0,0 +1,5 @@
+// this will ensure they're removed after each test.
+import 'react-testing-library/cleanup-after-each';
+
+// this adds jest-dom's custom assertions
+import '@testing-library/jest-dom/extend-expect';
