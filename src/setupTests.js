6b9d497b18c7279530d65cd4ce2f716f625a3626
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-12 14:37:24 -0500

    Removed the problematic configuration

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,2 @@
-// this will ensure they're removed after each test.
-import 'react-testing-library/cleanup-after-each';
-
 // this adds jest-dom's custom assertions
 import '@testing-library/jest-dom/extend-expect';
