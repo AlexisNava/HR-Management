8411c63ad76c417735bf7d286b1e3ff8012b8bb3
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-12 14:45:56 -0500

    Added tests to Not Found page

---
+++ src/pages/NotFound/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+// Component
+import NotFound from './index';
+
+it('should render the text `Not Found`', () => {
+  const { getByText } = render(<NotFound />);
+
+  expect(getByText('Not Found')).toBeInTheDocument();
+});
