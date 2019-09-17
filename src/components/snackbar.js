dbc62437383eb5a3323924dc070589cb75855d41
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-17 13:39:46 -0500

    Added snackbar component

---
+++ src/components/snackbar.js
@@ -0,0 +1,42 @@
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+
+import Snackbar from '@material-ui/core/Snackbar';
+import Button from '@material-ui/core/Button';
+
+// Action Creators
+import { hideNotification } from '../store/modules/notification/actionCreators';
+
+const snackbar = memo(({ isOpen, message }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <Snackbar
+      autoHideDuration={6000}
+      message={<span>{message}</span>}
+      open={isOpen}
+      onClose={() => dispatch(hideNotification())}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'left',
+      }}
+      action={[
+        <Button
+          color="secondary"
+          size="small"
+          onClose={() => dispatch(hideNotification())}
+        >
+          OK
+        </Button>,
+      ]}
+    />
+  );
+});
+
+snackbar.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  message: PropTypes.string.isRequired,
+};
+
+export default snackbar;
