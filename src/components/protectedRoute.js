ccc2846c2e355e3dcd797548fae16631be90a2f2
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-17 12:55:25 -0500

    Removed unnecesary code

--- src/components/protectedRoute.js
+++ src/components/protectedRoute.js
@@ -3,11 +3,7 @@ import { Route, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 function ProtectedRoute({ component: Component, ...rest }) {
-  const user = useSelector(state => state.user);
   const isLogged = useSelector(state => state.user.get('isLogged'));
-
-  console.log('user', user);
-  console.log('isLogged', isLogged);
 
   if (isLogged) {
     return <Route {...rest} component={Component} />;
