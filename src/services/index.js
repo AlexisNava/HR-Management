fbd8c74affcc3d706bf347eab30972d65a2e0bdd
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-17 19:47:23 -0500

    Changed the api host

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const logIn = async (email, password) => {
-  const response = await axios.post('http://localhost:4000/api/auth/login', {
+  const response = await axios.post('http://0.0.0.0:4000/api/auth/login', {
     email,
     password,
   });
