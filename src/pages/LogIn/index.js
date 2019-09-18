6db9d055c22026e745d78c6da56955d1df6660c7
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-18 08:00:09 -0500

    Added improvements to the login page

--- src/pages/LogIn/index.js
+++ src/pages/LogIn/index.js
@@ -26,63 +26,69 @@ const LogIn = memo(({ history }) => {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorPassword, setErrorPassword] = useState(false);
 
   return (
-    <div className="flex-container flex-container--center-content">
-      <Paper>
-        <div className="form flex-container flex-container--vertical-alignment">
-          <div className="form__element form__element--text">
-            <Typography variant="h5">HR Management</Typography>
-          </div>
-
-          <div className="form__element form__element--text">
-            <Typography variant="h6">Log In</Typography>
-          </div>
-
-          <div className="form__element">
-            <TextField
-              fullWidth
-              label="Email"
-              type="email"
-              margin="normal"
-              variant="outlined"
-              helperText="This field is required"
-              value={email}
-              onChange={event => setEmail(event.target.value)}
-            />
-          </div>
-
-          <div className="form__element">
-            <TextField
-              fullWidth
-              label="Password"
-              type="password"
-              margin="normal"
-              variant="outlined"
-              helperText="This field is required"
-              value={password}
-              onChange={event => setPassword(event.target.value)}
-            />
-          </div>
-
-          <div className="form__element">
-            <Button
-              fullWidth
-              variant="contained"
-              color="primary"
-              onClick={() => {
-                if (email && password) {
-                  dispatch(logIn(email, password));
-                } else {
-                }
-              }}
-            >
-              Log In
-            </Button>
-          </div>
+    <Paper className="form flex-container flex-container--center-content">
+      <div className="flex-container flex-container--vertical-alignment">
+        <div className="form__element form__element--text">
+          <Typography variant="h5">HR Management</Typography>
         </div>
-      </Paper>
-    </div>
+
+        <div className="form__element form__element--text">
+          <Typography variant="h6">Log In</Typography>
+        </div>
+
+        <div className="form__element">
+          <TextField
+            fullWidth
+            label="Email"
+            type="email"
+            margin="normal"
+            variant="outlined"
+            helperText="This field is required"
+            className="input--full-width"
+            error={errorEmail}
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+        </div>
+
+        <div className="form__element">
+          <TextField
+            fullWidth
+            label="Password"
+            type="password"
+            margin="normal"
+            variant="outlined"
+            helperText="This field is required"
+            className="input--full-width"
+            error={errorPassword}
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
+        </div>
+
+        <div className="form__element">
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              if (email && password) {
+                dispatch(logIn(email, password));
+              } else {
+                setErrorEmail(true);
+                setErrorPassword(true);
+              }
+            }}
+          >
+            Log In
+          </Button>
+        </div>
+      </div>
+    </Paper>
   );
 });
 
